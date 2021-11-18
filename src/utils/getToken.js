1422bf48e077a8110edacf98400af86d0453304f
Rajat Raj <rajatraj733@gmail.com>
2021-11-18 15:54:09 +0530

    made changes in env variables

--- src/utils/getToken.js
+++ src/utils/getToken.js
@@ -1,11 +1,12 @@
-const endPoint = process.env.REACT_APP_TOKEN_ENDPOINT;
-const room_id = process.env.REACT_APP_ROOM_ID;
+const endPoint = process.env.REACT_APP_TOKEN_ENDPOINT || 'https://webw2wje9f.execute-api.ap-south-1.amazonaws.com/';
+const room_id = process.env.REACT_APP_ROOM_ID || '61962136af3188df33e6935e';
+import uuidv4 from 'uuidv4'
 
 export default async function getToken(role) {
   const response = await fetch(`${endPoint}api/token`, {
     method: 'POST',
     body: JSON.stringify({
-      user_id: '5fc62c5872909272bf9995e1', // User ID assigned by you (different from 100ms' assigned id)
+      user_id: uuidv4.uuid(), // User ID assigned by you (different from 100ms' assigned id)
       role: role, // listener , speaker , moderator
       room_id,
     }),
