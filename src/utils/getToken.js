f2d91124b4a8f4e5d83f5119db42835c497396f4
Rajat Raj <rajatraj733@gmail.com>
2021-11-18 17:28:26 +0530

    fixed bugs

--- src/utils/getToken.js
+++ src/utils/getToken.js
@@ -1,16 +1,19 @@
-import uuidv4 from 'uuidv4';
+import {uuid} from 'uuidv4';
 
 const endPoint = process.env.REACT_APP_TOKEN_ENDPOINT || 'https://webw2wje9f.execute-api.ap-south-1.amazonaws.com/';
-const room_id = process.env.REACT_APP_ROOM_ID || '61962136af3188df33e6935e';
+const room_id = process.env.REACT_APP_ROOM_ID || '61962f65af3188df33e69396';
 
 export default async function getToken(role) {
   const response = await fetch(`${endPoint}api/token`, {
     method: 'POST',
     body: JSON.stringify({
-      user_id: uuidv4(), // User ID assigned by you (different from 100ms' assigned id)
+      user_id: uuid(), // User ID assigned by you (different from 100ms' assigned id)
       role: role, // listener , speaker , moderator
       room_id,
     }),
+    headers: {
+      'content-type': 'application/json'
+    }
   });
 
   const { token } = await response.json();
